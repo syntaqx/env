c62770822d2135095a78555dee9ed01db6ea91ce
John Peterson <peterson.glock17@gmail.com>
2024-07-15 15:28:07 -0600

    Add functionality for parsing comma list value into a slice of strings

--- env_test.go
+++ env_test.go
@@ -3,6 +3,8 @@ package env
 import (
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestGetWithFallback(t *testing.T) {
@@ -98,6 +100,69 @@ func TestGetBoolWithFallback(t *testing.T) {
 			if got != tt.wantValue {
 				t.Errorf("GetBoolWithFallback(%q, %v) = %v, want %v", tt.key, tt.fallback, got, tt.wantValue)
 			}
+			// Unset the environment variable
+			os.Unsetenv(tt.key)
+		})
+	}
+}
+func TestGetSliceWithFallback(t *testing.T) {
+	tests := []struct {
+		setValue  bool
+		key       string
+		fallback  []string
+		envValue  string
+		wantValue []string
+	}{
+		{
+			setValue:  true,
+			key:       "EXISTING_KEY_SLICE_ONE_VALUE",
+			fallback:  []string{"fallback_value_1"},
+			envValue:  "existing_value",
+			wantValue: []string{"existing_value"},
+		},
+		{
+			setValue:  false,
+			key:       "NON_EXISTING_KEY_SLICE",
+			fallback:  []string{"fallback_value"},
+			envValue:  "",
+			wantValue: []string{"fallback_value"},
+		},
+		{
+			setValue:  true,
+			key:       "EMPTY_VALUE_KEY_SLICE",
+			fallback:  []string{"fallback_value"},
+			envValue:  "",
+			wantValue: []string{""},
+		},
+		{
+			setValue:  true,
+			key:       "EXISTING_KEY_SLICE_MULTI_VALUE",
+			fallback:  []string{"fallback_value_1", "fallback_value_2"},
+			envValue:  "existing_value_1,existing_value_2",
+			wantValue: []string{"existing_value_1", "existing_value_2"},
+		},
+		{
+			setValue:  false,
+			key:       "NON_EXISTING_KEY_SLICE_MULTI_VALUE",
+			fallback:  []string{"fallback_value_1", "fallback_value_2"},
+			envValue:  "",
+			wantValue: []string{"fallback_value_1", "fallback_value_2"},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.key, func(t *testing.T) {
+			// Set the environment variable
+			if tt.setValue {
+				os.Setenv(tt.key, tt.envValue)
+			}
+
+			// Call the function under test
+			got := GetSliceWithFallback(tt.key, tt.fallback)
+
+			// Check if the returned value matches the expected value
+			assert.ElementsMatch(t, tt.wantValue, got, "GetSliceWithFallback(%q, %q) should return expected slice elements, order ignored", tt.key, tt.fallback)
+
 			// Unset the environment variable
 			os.Unsetenv(tt.key)
 		})
